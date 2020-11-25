f742145302e97f8261a6f07db900a77ad44dbc02
harshit-kakkar <kharshit0@gmail.com>
2020-11-25 15:29:08 +0530

    Initial setup

--- src/server.js
+++ src/server.js
@@ -1,2 +1,16 @@
-// Driver file
-//Setup done
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+
+app.listen(PORT, () => {
+    console.log("Server started")
+})
