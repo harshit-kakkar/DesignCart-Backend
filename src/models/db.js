14d23c9db6475c3e9c55975c696a241c6c77880c
Priyank <priyankmadan98@gmail.com>
2020-11-27 13:45:07 +0530

    customer table defined

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
- 
+
 const Sequelize = require('sequelize')
-const db = new Sequelize ('postgres', 'sportscartuser', 'sportscartpass', {
+const db = new Sequelize('postgres', 'sportscartuser', 'sportscartpass', {
     host: 'sportscart.ci8fuosldpou.ap-south-1.rds.amazonaws.com',
     port: 5432,
     dialect: 'postgres',
@@ -12,32 +12,65 @@ const Auth = db.define('auth', {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true
-      },
-    email:{
+    },
+    email: {
         type: Sequelize.STRING,
         allowNull: false,
         primaryKey: true
     },
-    pass_hash:{
+    pass_hash: {
         type: Sequelize.STRING,
         allowNull: false
     },
-    last_login:{
+    last_login: {
         type: Sequelize.DATE,
         allowNull: false
     },
-    mobile:{
-        type: Sequelize.STRING, 
-        allowNull: false, 
+    mobile: {
+        type: Sequelize.STRING,
+        allowNull: false,
         primaryKey: true
     },
-    is_mobile_verified:{
+    is_mobile_verified: {
         type: Sequelize.BOOLEAN
     },
-    is_email_verified:{
+    is_email_verified: {
         type: Sequelize.BOOLEAN
     },
 })
+
+const Auth = db.define('customer', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    firstname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    lastname: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    mobile: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        primaryKey: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    gender: {
+        type: Sequelize.STRING
+    },
+
+})
+
 
 module.exports = {
     db
