c7a84a4383a03c592e3b29d2eb5954935f5000b9
harshit-kakkar <kharshit0@gmail.com>
2020-11-27 13:21:10 +0530

    Database connection setup

--- src/models/db.js
+++ src/models/db.js
@@ -1,18 +1,43 @@
  
 const Sequelize = require('sequelize')
-const db = new Sequelize ('sportscart', 'sportscartuser', 'sportscartpass', {
+const db = new Sequelize ('postgres', 'sportscartuser', 'sportscartpass', {
     host: 'sportscart.ci8fuosldpou.ap-south-1.rds.amazonaws.com',
     port: 5432,
-    logging: console.log,
-    maxConcurrentQueries: 100,
     dialect: 'postgres',
-    dialectOptions: {
-        ssl:false
-    },
-    pool: { maxConnections: 5, maxIdleTime: 30},
-    language: 'en'
 })
 
+const Auth = db.define('auth', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    pass_hash:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    last_login:{
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    mobile:{
+        type: Sequelize.STRING, 
+        allowNull: false, 
+        primaryKey: true
+    },
+    is_mobile_verified:{
+        type: Sequelize.BOOLEAN
+    },
+    is_email_verified:{
+        type: Sequelize.BOOLEAN
+    },
+})
 
 module.exports = {
     db
