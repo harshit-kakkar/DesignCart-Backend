640a4523d027d7c434a97348b8a18e520ae8c27e
Priyank <priyankmadan98@gmail.com>
2020-12-03 19:59:24 +0530

    added login feature

---
+++ src/controllers/login.js
@@ -0,0 +1,42 @@
+const { Customer } = require('../models/db')
+const { Op } = require("sequelize")
+
+
+async function authenticate(customerDetails) {
+    let message = "Couldn't login user"
+    let status = 400
+    if (!customerDetails['email'] || !customerDetails["password"]) {
+        message = "validation error"
+        return { "message": message, "status": status }
+    }
+
+    customer = await Customer.findOne({
+        where: {
+            email: customerDetails['email']
+        }
+    })
+
+    if (!customer) {
+        message = "Wrond Username/password"
+        return { "message": message, "status": status }
+    }
+
+    if (customer.pass_hash !== customerDetails["password"]) {
+        message = "Wrond Username/password"
+        return { "message": message, "status": status }
+    }
+    message = "Customer logged-in successfully"
+    status = 200
+
+
+    return { "message": message, "status": status }
+
+}
+
+
+
+
+
+module.exports = {
+    authenticate
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,20 @@
 const { createCustomer } = require('../controllers/signup')
+const { authenticate } = require('../controllers/login')
+
+
+
 const route = require('express').Router()
 
 route.post('/signup', async (req, res) => {
     // Signup user the first time, with is_verified as false
     const customer_creation_obj = await createCustomer(req.body)
-    res.status(customer_creation_obj["status"]).send({"message":customer_creation_obj["message"]})
+    res.status(customer_creation_obj["status"]).send({ "message": customer_creation_obj["message"] })
+})
+
+route.post('/login', async (req, res) => {
+    //login user the first time , with is_verified as false
+    const customer_loggedIn_obj = await authenticate(req.body)
+    res.status(customer_loggedIn_obj["status"]).send({ "message": customer_loggedIn_obj["message"] })
 })
 
 
