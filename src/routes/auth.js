c198717d6e394724db90aed5e9ea421a0a43ab58
Priyank <priyankmadan98@gmail.com>
2020-12-06 13:39:48 +0530

    jwt authentication completed

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 const { createCustomer } = require('../controllers/signup')
 const { authenticate } = require('../controllers/login')
-
+const jwt = require('jsonwebtoken');
 
 
 const route = require('express').Router()
@@ -14,8 +14,80 @@ route.post('/signup', async (req, res) => {
 route.post('/login', async (req, res) => {
     //login user the first time , with is_verified as false
     const customer_loggedIn_obj = await authenticate(req.body)
-    res.status(customer_loggedIn_obj["status"]).send({ "message": customer_loggedIn_obj["message"] })
+    // res.status(customer_loggedIn_obj["status"]).send({ "message": customer_loggedIn_obj["message"] })
+    if (customer_loggedIn_obj["message"] == "validation error") {
+        res.json("Valdation error")
+    }
+    if (customer_loggedIn_obj["message"] == "Wrong Username/password") {
+        res.json("Wrong Username/password")
+    }
+
+
+    if (customer_loggedIn_obj["message"] == "Customer logged-in successfully") {
+        //jwt magic
+        jwt.sign(req.body, process.env.SECRET_KEY, { expiresIn: '2000s' }, (err, token) => {
+            res.json({
+                token
+            });
+        });
+    }
+
 })
+
+route.post("/protected", verifyToken, (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+            console.log(req.token)
+            console.log(err)
+
+        } else {
+            res.json({
+                message: "Authorized by jwt token",
+                authData
+
+            });
+        }
+    });
+
+})
+
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    console.log("header " + bearerHeader)
+    console.log("------------")
+    // Check if bearer is undefined
+    if (typeof bearerHeader !== 'undefined') {
+
+        // Split at the space
+        const bearer = bearerHeader.split(" ");
+
+        // // Get token from array
+        const bearerToken = bearer[1];
+
+        // Set the token
+        req.token = bearerToken;
+
+        // Next middleware
+        next();
+    } else {
+
+        // Forbidden
+        res.sendStatus(403);
+        console.log("Failed at token split")
+
+    }
+
+}
+
+
+
 
 
 module.exports = route
