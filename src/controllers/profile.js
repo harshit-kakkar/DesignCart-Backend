fa19d1d03b928cf73851e4f704f8a87678186947
harshit-kakkar <kharshit0@gmail.com>
2020-12-10 02:19:54 +0530

    Completed the implementation of get profile api

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -2,9 +2,21 @@ const { Customer } = require('../models/db')
 
 async function fetchProfile(email){
     let status = 200
+    let profileObj = {}
     let customer = await Customer.findOne({where: {email}})
-    console.log(customer.firstname)
-    return "hey"
+    if (customer){
+        profileObj["first_name"] = customer.firstname
+        profileObj["last_name"] = customer.lastname
+        profileObj["gender"] = customer.gender
+        profileObj["mobile"] = customer.mobile
+        profileObj["email"] = customer.email
+    }
+    else{
+        profileObj["message"] = "User doesn't exist"
+        status = 400
+    }
+
+    return {profileObj, status}
 }
 
 module.exports = {
--- src/routes/account.js
+++ src/routes/account.js
@@ -2,8 +2,15 @@ const route = require('express').Router()
 const { fetchProfile } = require('../controllers/profile')
 
 route.get('/profile', async (req, res) => {
-    let customerProfile = await fetchProfile("harshit@gmail.com")
-    res.status(200).send(customerProfile)
+    let req_email = ""
+    if (req.query.email){
+        req_email = req.query.email
+    }
+    else{
+        res.status(400).send({"message": "Email id required to fetch profile"})
+    }
+    let customerProfile = await fetchProfile(req_email)
+    res.status(customerProfile["status"]).send(customerProfile["profileObj"])
 })
 
 module.exports = route
